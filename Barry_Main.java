48b1921a356f2e1bd7502d187479b2a0f0f34cc5
jackjerrold <141284583+jackjerrold@users.noreply.github.com>
2026-03-08 23:17:57 +0000

    Update Barry_Main.java

--- Barry_Main.java
+++ Barry_Main.java
@@ -1,80 +1,136 @@
-@TeleOp(name = "Mecanum Drive TeleOp", group = "Drive")
-public class MecanumDriveTeleOp extends OpMode {
+package org.firstinspires.ftc.teamcode;
 
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import java.util.ArrayList;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
-    private DcMotor intakeMotor;
-    private DcMotor flywheelMotor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
 
-    private double speedMultiplier = 1.0;
+@TeleOp(name = "TeleOp 2026")
+public class TeleOp2026 extends OpMode{
 
-    private double flywheelPower = 90.0; 
+    AprilTagWebcam turretCam, sideCam;
+
+    private Blinker control_Hub;
+
+    private DcMotor leftFrontMotor;
+    private DcMotor rightFrontMotor;
+    private DcMotor leftBackMotor;
+    private DcMotor rightBackMotor;
+
+    private DcMotorEx accelMotor;
+    private CRServo turret;
+
+    private IMU imu;
+
+    private boolean active = true;
+
+    private double rotatedFromIdentity;
+
+    private int goalTagID = 0;
+    private int centerTagID = 1;
+    private int OppTagID = 2;
+
+    private double driveSpeed = 2;
 
     @Override
-    public void init() {
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+    public void init(){
 
-        intakeMotor   = hardwareMap.get(DcMotor.class, "intakeMotor");
-        flywheelMotor = hardwareMap.get(DcMotor.class, "flywheelMotor");
+        turretCam = new AprilTagWebcam();
+        sideCam = new AprilTagWebcam();
+        
+        hardwareInit(hardwareMap);
+        telemetry.addLine("Hardware Initialised");
+        turretCam.init(hardwareMap, telemetry,"Webcam 1");
+        sideCam.init(hardwareMap, telemetry, "Webcam 2");
+        telemetry.addLine("Cameras Initialised");
+        
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        flywheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        flywheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
+        telemetry.addLine("Press to start...");
     }
 
     @Override
-    public void loop() {
+    public void loop(){
 
-        if (gamepad1.left_stick_y > 0.1 || gamepad1.right_stick_y > 0.1) {
-            double y  = gamepad1.left_stick_y;
-            double x  =  -gamepad1.left_stick_x;
-            double rx =  gamepad1.right_stick_x;
+        if (gamepad1.dpadDownWasPressed()){active = !active;}
+
+        if (active){
+
+            turretCam.update();
+            turretUpdate();
+            rotatedFromIdentity = getRotation();
+            
+    
+        
+        }
+        else{telemetry.addLine("Press Dpad-Down to activate");}
+    }
+
+    public void hardwareInit(HardwareMap hdwr){
+        turret = hdwr.get(CRServo.class, "servo1");
+
+        leftFrontMotor = hdwr.get(DcMotor.class, "lf");
+        leftBackMotor = hdwr.get(DcMotor.class, "lb");
+        rightFrontMotor = hdwr.get(DcMotor.class, "rf");
+        rightBackMotor = hdwr.get(DcMotor.class, "rb");
+
+        imu = hardwareMap.get(IMU.class, "IMU");
+        imu.resetYaw();
+        
+    }
+    
+    public void turretUpdate(){
+        AprilTagDetection goalTag = turretCam.getTagBySpecificId(goalTagID);
+        
+        if(goalTag != null){
+            double power = -goalTag.ftcPose.bearing/10;
+            turret.setPower(power);
+            telemetry.addData("Servo Power", power);
+        }   
+    }
+    
+    public double getRotation(){
+        AprilTagDetection centerTag = sideCam.getTagBySpecificId(centerTagID);
+        AprilTagDetection goalTag = sideCam.getTagBySpecificId(goalTagID);
+        AprilTagDetection OppTag = sideCam.getTagBySpecificId(OppTagID);
+
+        if (centerTag == null && goalTag == null && OppTag == null){
+
+        }
+
+        ArrayList<Double> angles = new ArrayList<Double>();
+
+        if(centerTag != null){
+            angles.add(centerTag.ftcPose.bearing - Math.PI/2);
         }
         
-        double frontLeftPower  = y + x + rx;
-        double backLeftPower   = y - x + rx;
-        double frontRightPower = y - x - rx;
-        double backRightPower  = y + x - rx;
-
-        double max = Math.max(
-                Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)),
-                Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))
-        );
-
-        if (max > 1.0) {
-            frontLeftPower  /= max;
-            frontRightPower /= max;
-            backLeftPower   /= max;
-            backRightPower  /= max;
+        
+        if(goalTag != null){
+            angles.add(goalTag.ftcPose.bearing - Math.PI/4);
         }
 
-        frontLeft.setPower(frontLeftPower * speedMultiplier);
-        frontRight.setPower(frontRightPower * speedMultiplier);
-        backLeft.setPower(backLeftPower * speedMultiplier);
-        backRight.setPower(backRightPower * speedMultiplier);
-
-        intakeMotor.setPower(gamepad1.left_trigger);
-
-        if (gamepad1.right_trigger > 0.1) {
-            flywheelMotor.setPower(-flywheelPower);
-        } else {
-            flywheelMotor.setPower(0);
+        
+        if(OppTag != null){
+            angles.add(OppTag.ftcPose.bearing - Math.PI*3/4);
         }
+
+        double totalAngle = 0;
+        for (int i = 0; i < angles.size(); i++) {
+            totalAngle += angles.get(i);
+        }
+
+        return (totalAngle/angles.size());
     }
 }
-
